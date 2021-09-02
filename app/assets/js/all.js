029716eb26033fc70dccd28c6b8eaca0a1508c42
wafflefrank <83715021+wafflefrank@users.noreply.github.com>
2021-09-03 00:25:22 +0800

    9/3 update

---
+++ app/assets/js/all.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  var swiper = new Swiper(".swiper", {
+   slidesPerView: 1,
+   spaceBetween: 30,
+   breakpoints: {
+     768:{
+       slidesPerView: 3,
+       spaceBetween: 30,
+     },
+   },
+   scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+ });
+});
